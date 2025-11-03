78646bab02c70417f064286e3b667b145de7b4de
AkhatorEnosa <aosakhogba@gmail.com>
2025-11-03 09:21:13 +0100

    updated cron package

--- src/config/cron.js
+++ src/config/cron.js
@@ -5,7 +5,7 @@ import https from "https";
 const job = cron.schedule("* * * * *", function () {
   https
     .get(process.env.API_URL, (res) => {
-      if (res.statusCode === 200) console.log("GET request sent successfully");
+      if (res.statusCode === 200) console.log("GET request for api/health endpoint sent successfully using cron");
       else console.log("GET request failed", res.statusCode);
     })
     .on("error", (e) => console.error("Error while sending request", e));
