329b3cc3675e7cb084944b931388186b61a1ca69
AkhatorEnosa <aosakhogba@gmail.com>
2025-11-05 11:53:40 +0100

    updated cron

--- src/config/cron.js
+++ src/config/cron.js
@@ -6,7 +6,7 @@ const job = cron.schedule("*/13 * * * *", function () {
   https
     .get(process.env.API_URL, (res) => {
       if (res.statusCode === 200) console.log("GET request for api/health endpoint sent successfully using cron");
-      else console.log("GET request for api/health endpoint sent successfully using cron", res.statusCode);
+      else console.log("GET request for api/health endpoint failed using cron", res.statusCode);
     })
     .on("error", (e) => console.error("Error while sending request for api/health endpoint sent successfully using cron", e));
 });
